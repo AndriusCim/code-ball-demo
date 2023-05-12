8645e83979e8fc2b562b27d114bf577e9219efaa
andrius.cimermonas <andrius.cimermonas@oxylabs.io>
2023-05-12 10:38:23 +0300

    update header color

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 style={{ color: "red" }}>Vite + React</h1>
       <h2>New title</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
